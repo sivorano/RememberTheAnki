cdf3d336059edb5ec8cfe276926838b133b6b921
Anders <anders@pop-os.localdomain>
2019-06-23 12:18:35 +0200

    Added the basic file.

---
+++ RememberTheAnki.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+
+
+import sys
+import socket
+from anki import Collection
+from shutil import copyfile,rmtree
+from anki.utils import intTime
+import time
+import os,glob
+import fileinput
+import logging
+import threading
+import time
+import socket
+from contextlib import closing
+import subprocess
+
+BUFFER_SIZE = 1024
+
+
+def findFreePort():
+    """
+    returns an usnused port, see 
+    https://stackoverflow.com/questions/1365265/on-localhost-how-do-i-pick-a-free-port-number
+    """
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('localhost', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
+
+def RecieveStdInInput():
+    """
+    Check if there is input from StdIn. Returns this input as an list.
+    """
+    args = []
+    return sys.argv
+
+def DumThread():
+    while True:
+        time.sleep(2)
+
+
+
+
+def StartBackgroundServer(portId):
+    """
+    Starts the background server, which checks 
+    """
+    def ServerThread(portId):
+        s = socket.socket(socket.AF_INET,SOCK_STREAM)
+        s.bind(("127.0.0.1",portId))
+
+        while True:
+            s.listen(1)
+            conn,addr = s.accept()
+            while True:
+                data = conn.recv(BUFFER_SIZE)
+                if not data: break
+                #print ("received data:", data)
+                conn.send(data)  # echo    ()
+            
+
+def HandleInput(args):
+    if len(args) <= 1:
+        print ("ERROR: Program needs atleast 1 argument, eg startup")
+        return 0
+    elif args[1] == "startup":
+        print("Runing startup")
+        return 1
+    elif args[1] == "close":
+        return 1
+    elif args[1] == "recheck":
+        return 1
+    elif args[1] == "isruning":
+        return 1
+    elif args[1] == "repeat":
+        return 1
+    else:
+        print("Invalid input")
+        return 0
+    
+
+if __name__ == "__main__":
+    # We start by finding the input arguments
+
+    InputArgs = RecieveStdInInput()
+    print(InputArgs)
+    reactionCode = HandleInput(InputArgs)
+
+    if reactionCode == 0:
+        sys.exit()
+
+
+
+# def thread_function(name):
+#     logging.info("Thread %s: starting", name)
+#     time.sleep(2)
+#     logging.info("Thread %s: finishing", name)
+
+# if __name__ == "__main__":
+#     format = "%(asctime)s: %(message)s"
+#     logging.basicConfig(format=format, level=logging.INFO,
+#                         datefmt="%H:%M:%S")
+
+#     logging.info("Main    : before creating thread")
+#     x = threading.Thread(target=thread_function, args=(1,))
+#     logging.info("Main    : before running thread")
+#     x.start()
+#     logging.info("Main    : wait for the thread to finish")
+#     x.join()
+#     logging.info("Main    : all done")
+
+
